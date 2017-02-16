87ed29bdb55a2f0e7d4f3219062df1d46c951053
Avi Weinstock <aweinstock314@gmail.com>
2017-02-16 13:55:16 -0500

    Added an empty interrupt handler. Added printouts to the /dev/pseudohsm handler.

--- PseudoHSM.c
+++ PseudoHSM.c
@@ -4,6 +4,7 @@
 
 #include <linux/fs.h>        
 #include <linux/miscdevice.h>
+#include <linux/interrupt.h>
 #include <linux/string.h>    
 #include <linux/random.h>    
 
@@ -11,8 +12,19 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Avi Weinstock");
 MODULE_DESCRIPTION("PseudoHSM");
 
+#define PSEUDOHSM_IRQ 0x7f
+
+static irqreturn_t interrupt_handler(int irq, void *dev_id) {
+    return 0;
+}
+
 static ssize_t store_key(struct file* file, const char * buf, size_t count, loff_t *ppos) {
     // TODO: implement
+    size_t i;
+    printk(KERN_INFO "store_key: buf: %s\n", buf);
+    for(i=0; i < count; i++) {
+        printk("buf[%lu]: %hhx", i, buf[i]);
+    }
     return count;
 }
 
@@ -30,7 +42,8 @@ static int __init pseudohsm_init(void) {
     printk(KERN_INFO "PseudoHSM::pseudohsm_init\n");
     rc = misc_register(&pseudohsm_dev);
     printk(KERN_INFO "PseudoHSM: misc_register returned %d\n", rc);
-    // TODO: interrupt handler(s)
+    rc = request_irq(PSEUDOHSM_IRQ, interrupt_handler, 0, "pseudohsm_interrupt", 0);
+    printk(KERN_INFO "PseudoHSM: request_irq returned %d\n", rc);
     return 0;
 }
 
@@ -39,6 +52,7 @@ static void __exit pseudohsm_exit(void) {
     printk(KERN_INFO "PseudoHSM::pseudohsm_exit\n");
     rc = misc_deregister(&pseudohsm_dev);
     printk(KERN_INFO "PseudoHSM: misc_deregister returned %d\n", rc);
+    free_irq(PSEUDOHSM_IRQ, 0);
 }
 
 module_init(pseudohsm_init);
