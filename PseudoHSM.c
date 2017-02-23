c2fa376a09649aaef793184689f9f485d308c4fa
Avi Weinstock <aweinstock314@gmail.com>
2017-02-23 11:48:56 -0500

    Add cache toggling code (doesn't yet work due to 64 and 32 bit mismatches). Add a few commented-out attempts to register a nonstandard software interrupt (needs more research into the relevant kernel structures).

--- PseudoHSM.c
+++ PseudoHSM.c
@@ -1,18 +1,19 @@
 #include <linux/module.h>
-#include <linux/kernel.h>    
-#include <linux/init.h>      
+#include <linux/kernel.h>
+#include <linux/init.h>
 
-#include <linux/fs.h>        
-#include <linux/miscdevice.h>
+#include <linux/fs.h>
 #include <linux/interrupt.h>
-#include <linux/string.h>    
-#include <linux/random.h>    
+#include <linux/irqdomain.h>
+#include <linux/miscdevice.h>
+#include <linux/random.h>
+#include <linux/string.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Avi Weinstock");
 MODULE_DESCRIPTION("PseudoHSM");
 
-#define PSEUDOHSM_IRQ 0x7f
+#define PSEUDOHSM_IRQ 0x22
 
 static irqreturn_t interrupt_handler(int irq, void *dev_id) {
     return 0;
@@ -37,12 +38,65 @@ static struct miscdevice pseudohsm_dev = {
     MISC_DYNAMIC_MINOR, "pseudohsm" , &pseudohsm_ops
 };
 
+static int disable_cache(void) {
+    printk(KERN_ALERT "Disabling L1 and L2 caches.\n");
+    /*__asm__(".intel_syntax noprefix\n\t"
+            "mov    eax,cr0\n\t"
+            "or     eax,(1 << 30)\n\t"
+            "mov    cr0,eax\n\t"
+            "wbinvd\n\t"
+            ".att_syntax noprefix\n\t"
+    : : : "eax" );*/
+    return 0;
+}
+static void enable_cache(void) {
+    printk(KERN_ALERT "Enabling L1 and L2 caches.\n");
+    /*__asm__(".intel_syntax noprefix\n\t"
+            "mov    eax,cr0\n\t"
+            "and     eax,~(1 << 30)\n\t"
+            "mov    cr0,eax\n\t"
+            "wbinvd\n\t"
+            ".att_syntax noprefix\n\t"
+    : : : "eax" );*/
+}
+
+static ssize_t toggle_cache(struct file* file, const char * buf, size_t count, loff_t *ppos) {
+    if(count != 1) {
+        return -EMSGSIZE;
+    }
+    switch(*buf) {
+        default:
+            return -EBADRQC;
+        case '0':
+            disable_cache();
+            return 1;
+        case '1':
+            enable_cache();
+            return 1;
+    }
+}
+
+static const struct file_operations cache_ops = {
+    .owner  = THIS_MODULE,
+    .write  = toggle_cache,
+};
+
+static struct miscdevice cache_dev = {
+    MISC_DYNAMIC_MINOR, "set_cpu_cache" , &cache_ops
+};
+
 static int __init pseudohsm_init(void) {
-    int rc;
+    int rc/*, virq*/;
     printk(KERN_INFO "PseudoHSM::pseudohsm_init\n");
     rc = misc_register(&pseudohsm_dev);
-    printk(KERN_INFO "PseudoHSM: misc_register returned %d\n", rc);
-    rc = request_irq(PSEUDOHSM_IRQ, interrupt_handler, 0, "pseudohsm_interrupt", 0);
+    printk(KERN_INFO "PseudoHSM: misc_register(&pseudohsm_dev) returned %d\n", rc);
+    rc = misc_register(&cache_dev);
+    printk(KERN_INFO "PseudoHSM: misc_register(&cache_dev) returned %d\n", rc);
+    //rc = irq_alloc_desc_at(PSEUDOHSM_IRQ, 0);
+    //printk(KERN_INFO "PseudoHSM: irq_alloc_desc_at returned %d\n", rc);
+    //virq = irq_create_mapping(0, PSEUDOHSM_IRQ);
+    //printk(KERN_INFO "PseudoHSM: irq_create_mapping(0, %d) returned %d\n", PSEUDOHSM_IRQ, virq);
+    rc = request_irq(PSEUDOHSM_IRQ, interrupt_handler, IRQF_TRIGGER_RISING, "pseudohsm_interrupt", 0);
     printk(KERN_INFO "PseudoHSM: request_irq returned %d\n", rc);
     return 0;
 }
@@ -51,8 +105,11 @@ static void __exit pseudohsm_exit(void) {
     int rc;
     printk(KERN_INFO "PseudoHSM::pseudohsm_exit\n");
     rc = misc_deregister(&pseudohsm_dev);
-    printk(KERN_INFO "PseudoHSM: misc_deregister returned %d\n", rc);
+    printk(KERN_INFO "PseudoHSM: misc_deregister(&pseudohsm_dev) returned %d\n", rc);
+    rc = misc_deregister(&cache_dev);
+    printk(KERN_INFO "PseudoHSM: misc_deregister(&cache_dev) returned %d\n", rc);
     free_irq(PSEUDOHSM_IRQ, 0);
+    //irq_free_desc(PSEUDOHSM_IRQ);
 }
 
 module_init(pseudohsm_init);
---
+++ interrupt_tester.c
@@ -0,0 +1,1 @@
+const char main[] = "\xcd\x22\xc3";
